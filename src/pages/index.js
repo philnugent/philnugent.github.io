264bc8860bbca0cc3c64c3c13fc8c0971de62f7a
Phil  Nugent <phil@Phils-MacBook-Pro.local>
2021-03-13 13:43:12 -0500

    changed template

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,20 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
+    <h1>Phil Nugent</h1>
+    <p>Personal sandbox of Phil Nugent</p>
+    {/* <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
+    /> */}
+    {/* <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    </p> */}
   </Layout>
 )
 
